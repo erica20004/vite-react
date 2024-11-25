c0cae37c00ba7341748fa6dcc82640157c58126e
erica20004 <dogbatseerica128@gmail.com>
2024-11-25 12:03:09 +0000

    updated publick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header({title, subTitle, bgImage}) {
     return(
-        <header className="masthead" style={{backgroundImage:`url('/src/assets/img/${bgImage}')`}}>
+        <header className="masthead" style={{backgroundImage:`url('assets/img/${bgImage}')`}}>
             <div className="container position-relative px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
                     <div className="col-md-10 col-lg-8 col-xl-7">
